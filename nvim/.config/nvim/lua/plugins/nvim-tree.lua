e274c0a34156d77b4646e3f953cb6004bddf776d
Matthew Kavanagh <matthewfkavanagh@icloud.com>
2025-01-01 20:20:46 -0500

    Show dotfiles and gitignore in nvim-tree

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -6,7 +6,16 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup({
+      git = {
+        enable = true,
+        ignore = false,
+        timeout = 500,
+      },
+      filters = {
+        dotfiles = false,
+      }
+    })
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
       -- Toggle nvim-tree with Ctrl-N
