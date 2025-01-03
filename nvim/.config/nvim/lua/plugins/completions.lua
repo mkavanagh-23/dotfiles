b790b027ceb042708d5f15ce1eae96d59f788ea6
Matthew Kavanagh <matthewfkavanagh@icloud.com>
2025-01-03 13:04:21 -0500

    Improve completions and snippet integration

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -14,7 +14,7 @@ return {
     "hrsh7th/nvim-cmp",
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()    -- Load VSCode-like snippets
-
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
       cmp.setup({
         snippet = {
@@ -32,7 +32,40 @@ return {
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          -- ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<CR>'] = cmp.mapping(function(fallback)
+               if cmp.visible() then
+                   if luasnip.expandable() then
+                       luasnip.expand()
+                   else
+                       cmp.confirm({
+                           select = true,
+                       })
+                   end
+               else
+                   fallback()
+               end
+           end),
+
+           ["<Tab>"] = cmp.mapping(function(fallback)
+             if cmp.visible() then
+               cmp.select_next_item()
+             elseif luasnip.locally_jumpable(1) then
+               luasnip.jump(1)
+             else
+               fallback()
+             end
+           end, { "i", "s" }),
+
+           ["<S-Tab>"] = cmp.mapping(function(fallback)
+             if cmp.visible() then
+               cmp.select_prev_item()
+             elseif luasnip.locally_jumpable(-1) then
+               luasnip.jump(-1)
+             else
+               fallback()
+             end
+           end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
