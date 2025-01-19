d14ce1868f7aaa056a46fc2a8bf81e594c80a26e
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-01-19 11:09:41 -0500

    [NVIM] Auto-close HTML tags

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -189,4 +189,20 @@ return {
       })
     end,
   },
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup({
+        opts = {
+          -- Defaults
+          enable_close = true,         -- Auto close tags
+          enable_rename = true,        -- Auto rename pairs of tags
+          enable_close_on_slash = true -- Auto close on trailing </
+        },
+        -- Also override individual filetype configs, these take priority.
+        -- Empty by default, useful if one of the "opts" global settings
+        -- doesn't work well in a specific filetype
+      })
+    end
+  }
 }
