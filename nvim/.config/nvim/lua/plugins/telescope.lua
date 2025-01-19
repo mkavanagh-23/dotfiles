8258b46e577748682a8526bb14b6119f54ce22fc
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-01-19 10:54:16 -0500

    Add emoji picker

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -25,5 +25,28 @@ return {
       })
       require("telescope").load_extension("ui-select")
     end
+  },
+  {
+    'xiyaowong/telescope-emoji.nvim',
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          emoji = {
+            action = function(emoji)
+              -- argument emoji is a table.
+              -- {name="", value="", cagegory="", description=""}
+
+              vim.fn.setreg("*", emoji.value)
+              print([[Press p or "*p to paste this emoji]] .. emoji.value)
+
+              -- insert emoji when picked
+              vim.api.nvim_put({ emoji.value }, 'c', false, true)
+            end,
+          }
+        },
+      }
+      require("telescope").load_extension("emoji")
+      vim.keymap.set('n', '<leader>ef', ':Telescope emoji<CR>', { desc = "Open emoji picker" })
+    end
   }
 }
