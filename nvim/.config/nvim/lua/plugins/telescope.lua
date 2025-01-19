2003e55169c17c5ec3b1d3f3d00b26aef584de9d
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-01-19 11:17:48 -0500

    [NVIM] Add whichkey to view keymaps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -48,5 +48,24 @@ return {
       require("telescope").load_extension("emoji")
       vim.keymap.set('n', '<leader>ef', ':Telescope emoji<CR>', { desc = "Open emoji picker" })
     end
+  },
+  -- View currently set keymaps
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
   }
 }
