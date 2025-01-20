69f715660b79a9fce3deb37b0bc0e9c5ea51d2e5
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-01-19 21:45:48 -0500

    Create undo-tree persistence

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -1,6 +1,17 @@
 return {
   {
     "mbbill/undotree",
-    vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle, { desc = "Undo tree toggle" })
+    vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle, { desc = "Undo tree toggle" }),
+    config = function()
+      if vim.fn.has("persistent_undo") == 1 then
+        local target_path = vim.fn.expand("~/.undodir")
+        -- Create the directory if it doesn't exist
+        if vim.fn.isdirectory(target_path) == 0 then
+          vim.fn.mkdir(target_path, "p", 0700)
+        end
+        vim.opt.undodir = target_path
+        vim.opt.undofile = true
+      end
+    end,
   },
 }
