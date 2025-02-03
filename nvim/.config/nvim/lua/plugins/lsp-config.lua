481db98ab0b1fa6d6e280bf60c22fbbf3e0a7ccb
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-02-03 15:39:44 -0500

    Remove auto-formatting and pop-up notifications

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -34,13 +34,13 @@ return {
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover details" })
 
         -- Auto-format on save
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = vim.api.nvim_create_augroup("LspAutoFormat", { clear = true }),
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({ async = true })
-          end,
-        })
+        --vim.api.nvim_create_autocmd("BufWritePre", {
+        --  group = vim.api.nvim_create_augroup("LspAutoFormat", { clear = true }),
+        --  buffer = bufnr,
+        --  callback = function()
+        --    vim.lsp.buf.format({ async = true })
+        --  end,
+        --})
       end
 
       lspconfig.lua_ls.setup({
--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -11,7 +11,7 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+    -- "rcarriga/nvim-notify",
   },
   config = function()
     require("noice").setup({
@@ -71,8 +71,8 @@ return {
         lsp_doc_border = true,        -- add a border to hover docs and signature help
       },
     })
-    require("notify").setup({
-      background_colour = "#000000",
-    })
+    --require("notify").setup({
+    --  background_colour = "#000000",
+    --})
   end
 }
