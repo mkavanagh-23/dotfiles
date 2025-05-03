4c441dc28e390db07b4f349650471d682f6b484b
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-05-03 13:50:09 -0400

    Backup

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "bashls", "clangd", "cssls", "dockerls", "docker_compose_language_service", "arduino_language_server", "html", "hyprls", "sqls" }
+        ensure_installed = { "lua_ls", "bashls", "clangd", "cssls", "dockerls", "docker_compose_language_service", "arduino_language_server", "html", "hyprls", "sqls", "pylsp" }
       })
     end
   },
@@ -82,6 +82,10 @@ return {
         capabilities = capabilities
       })
       lspconfig.sqls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities
+      })
+      lspconfig.pylsp.setup({
         on_attach = on_attach,
         capabilities = capabilities
       })
