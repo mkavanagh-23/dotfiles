7604897655914bb3314675c988c1fae39658ffff
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-05-19 15:01:19 -0400

    Update scripts

--- scripts/.scripts/vpn-status.sh
+++ scripts/.scripts/vpn-status.sh
@@ -2,9 +2,9 @@
 
 if ip link show nordlynx > /dev/null 2>&1; then
   vpn_status=$(nordvpn status | sed ':a;N;$!ba;s/\n/\\n/g; s/"/\\"/g')
-  echo "{\"text\": \"󰦝\", \"class\": \"connected\", \"tooltip\": \"$vpn_status\"}"
+  echo "{\"text\": \"󰦝\", \"class\": \"connected\", \"tooltip\": \"VPN $vpn_status\"}"
 else
   vpn_status=$(nordvpn status | head -n 1)
-  echo "{\"text\": \"󱦛\", \"class\": \"disconnected\", \"tooltip\": \"$vpn_status\"}"
+  echo "{\"text\": \"󱦛\", \"class\": \"disconnected\", \"tooltip\": \"VPN $vpn_status\"}"
 fi
 
