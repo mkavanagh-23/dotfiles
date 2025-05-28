ffa0426cdd3c7bee58c7542563691e946bf4989f
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-05-28 09:36:45 -0400

    Config update

--- nvim/.config/nvim/lua/plugins/nvim-highlight-colors.lua
+++ nvim/.config/nvim/lua/plugins/nvim-highlight-colors.lua
@@ -8,20 +8,20 @@ return {
         render = 'virtual',
 
         ---Set virtual symbol (requires render to be set to 'virtual')
-        virtual_symbol = '■',
+        virtual_symbol = '󱓻',
 
         ---Set virtual symbol suffix (defaults to '')
-        virtual_symbol_prefix = '',
+        virtual_symbol_prefix = ' ',
 
         ---Set virtual symbol suffix (defaults to ' ')
-        virtual_symbol_suffix = ' ',
+        virtual_symbol_suffix = '',
 
         ---Set virtual symbol position()
         ---@usage 'inline'|'eol'|'eow'
         ---inline mimics VS Code style
         ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
         ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
-        virtual_symbol_position = 'inline',
+        virtual_symbol_position = 'eow',
 
         ---Highlight hex colors, e.g. '#FFFFFF'
         enable_hex = true,
