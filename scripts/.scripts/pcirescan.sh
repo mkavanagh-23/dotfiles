15175df93bb443a07c332fcc6ebe28f71ca0ab16
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-06-03 14:17:58 -0400

    Fix pci script

--- scripts/.scripts/pcirescan.sh
+++ scripts/.scripts/pcirescan.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # A script to rescan pci devices, run as root
 
+USER_CALLER="${SUDO_USER:-$LOGNAME}"
+
 echo 1 | tee /sys/bus/pci/rescan > /dev/null
 if [[ $? == 0 ]]; then
   echo "PCI devices rescanned successfully."
+  runuser -u $USER_CALLER -- notify-send " System" "PCI devices rescanned"
   exit 0
 fi
 
